b51607fb9dd5665964750e9dda13eeea27311250
HUON Sopanh@ <panhabro089@gmail.com>
2024-12-23 21:10:31 +0700

    Add files via upload

---
+++ diabetes_prediction/diabetes_prediction/user/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+class DiabetesPrediction(models.Model):
+    SEX_CHOICES = [
+        ('male', 'Male'),
+        ('female', 'Female'),
+    ]
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    sex = models.CharField(max_length=10, choices=SEX_CHOICES)
+    age = models.IntegerField()
+    glucose = models.FloatField()
+    skin_thickness = models.FloatField()
+    num_pregnancies = models.IntegerField()
+    bmi = models.FloatField()
+    blood_pressure = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+# Create your models here.
